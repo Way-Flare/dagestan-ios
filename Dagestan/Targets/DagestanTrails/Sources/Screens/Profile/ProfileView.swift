6fbcc454a116ce1ddb2e7ecb932f5d725ed14a4a
Gleb Rasskazov <rasskazov@1221systems.com>
2024-07-26 13:21:19 +0300

    DT-261: Profile UI

--- Dagestan/Targets/DagestanTrails/Sources/Screens/Profile/ProfileView.swift
+++ Dagestan/Targets/DagestanTrails/Sources/Screens/Profile/ProfileView.swift
@@ -6,40 +6,102 @@
 //  Copyright © 2024 WayFlare.com. All rights reserved.
 //
 
+import DesignSystem
 import SwiftUI
 
 struct ProfileView: View {
+    @State var offset: CGFloat = .zero
+
     var body: some View {
-        StickyHeaderView()
-            .edgesIgnoringSafeArea(.top)
+        ScrollViewWithScrollOffset(scrollOffset: $offset) {
+            VStack(spacing: .zero) {
+                ProfileImageView(offset: $offset)
+                menuSection
+                    .overlay(
+                        profileCircle,
+                        alignment: .top
+                    )
+                    .offset(y: -24)
+            }
+        }
+        .coordinateSpace(name: "pullToResize")
+        .ignoresSafeArea()
+        .background(WFColor.surfaceSecondary, ignoresSafeAreaEdges: .all)
+
+    }
+    
+    private var profileCircle: some View {
+        VStack(spacing: 8) {
+            Circle()
+                .fill(WFColor.surfacePrimary)
+                .frame(width: 96, height: 96)
+                .overlay(
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 28, height: 28)
+                        .foregroundStyle(WFColor.iconSoft)
+                )
+                .overlay(
+                    Circle()
+                        .strokeBorder(WFColor.surfaceSecondary, lineWidth: 2)
+                )
+            
+            Text("[username]")
+                .foregroundColor(WFColor.foregroundPrimary)
+                .font(.manropeSemibold(size: 18))
+        }
+        .offset(y: -65)
+    }
+
+    private var menuSection: some View {
+        VStack(spacing: 10) {
+            MenuItemView(icon: DagestanTrailsAsset.starOutline.swiftUIImage, title: "Мои отзывы")
+            MenuItemView(icon: DagestanTrailsAsset.profileCircle.swiftUIImage, title: "Управление аккаунтом")
+            MenuItemView(icon: DagestanTrailsAsset.logout.swiftUIImage, title: "Выйти")
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top, 79)
+        .padding(.horizontal, 12)
+        .background(WFColor.surfaceSecondary)
+        .cornerRadius(12)
     }
 }
 
-struct StickyHeaderView: View {
-    var body: some View {
-        ScrollView {
-            GeometryReader { geometry in
-                let minY = geometry.frame(in: .global).minY
-                let scale = max(1, 1 + minY / 500)
-                
-                DagestanTrailsAsset.profileBg.swiftUIImage
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: UIScreen.main.bounds.width, height: 200)
-                    .clipped()
-                    .scaleEffect(scale, anchor: .top)
-                    .offset(y: minY > 0 ? minY / scale : 0)
-            }
-            .frame(height: 200)
-            .edgesIgnoringSafeArea(.top) // Игнорируем верхнюю safe area
+struct ProfileImageView: View {
+    @Binding var offset: CGFloat
 
-            VStack(alignment: .leading) {
-                ForEach(0..<100) { i in
-                    Text("Item \(i)")
-                        .padding()
-                }
-            }
+    var body: some View {
+        GeometryReader { proxy in
+            DagestanTrailsAsset.profileBg.swiftUIImage
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .padding(.horizontal, min(0, -offset))
+                .frame(width: proxy.size.width,
+                       height: proxy.size.height + max(0, offset))
+                .offset(CGSize(width: 0, height: min(0, -offset)))
         }
+        .aspectRatio(CGSize(width: 375, height: 280), contentMode: .fit)
+    }
+}
+
+struct MenuItemView: View {
+    var icon: Image
+    var title: String
+
+    var body: some View {
+        HStack(spacing: 10) {
+            icon
+                .resizable()
+                .frame(width: 24, height: 24)
+                .foregroundColor(.green)
+            Text(title)
+                .foregroundColor(WFColor.foregroundPrimary)
+                .font(.manropeRegular(size: 16))
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(WFColor.surfacePrimary)
+        .cornerRadius(10)
     }
 }
 
